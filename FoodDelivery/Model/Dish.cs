edd0a0852048a10d69f330c1624168dadc18038d
Aykut5647 <aykutfeyzoliu5647@gmail.com>
2026-06-12 13:16:11 +0300

    DataBaseOnly

---
+++ FoodDelivery/Model/Dish.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodDelivery.Model
+{
+    public class Dish
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public double Grammage { get; set; }
+        public int DishTypeId { get; set; }
+        public DishType DishTypes { get; set; }
+    }
+}
---
+++ FoodDelivery/Model/DishesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodDelivery.Model
+{
+    internal class DishesContext:DbContext
+    {
+        public DishesContext() : base("DishesContext")
+        {
+
+        }
+        public DbSet<Dish> Dishes { get; set; }
+        public DbSet<DishType> DishTypes { get; set; }
+    }
+}
